9bc5a17640fd15bd3597ce6f129e00b867b04a11
nathan-hawkins <natehawk@live.com>
2022-07-09 08:00:56 -0500

    Initial commit.

---
+++ src/main/java/com/hawkins/simpletimeclock/SimpleTimeClockApplication.java
@@ -0,0 +1,15 @@
+package com.hawkins.simpletimeclock;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SimpleTimeClockApplication
+{
+	
+	public static void main(String[] args)
+	{
+		SpringApplication.run(SimpleTimeClockApplication.class, args);
+	}
+	
+}
---
+++ src/test/java/com/hawkins/simpletimeclock/SimpleTimeClockApplicationTests.java
@@ -0,0 +1,16 @@
+package com.hawkins.simpletimeclock;
+
+import org.junit.jupiter.api.Test;
+import org.junit.jupiter.api.extension.ExtendWith;
+import org.mockito.junit.jupiter.MockitoExtension;
+
+@ExtendWith(MockitoExtension.class)
+class SimpleTimeClockApplicationTests
+{
+	
+	@Test
+	void contextLoads()
+	{
+	}
+	
+}
